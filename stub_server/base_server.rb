38828b9c5fcd27e7cedd4224b4737b599b8b61da
SKunath <skunath@gmail.com>
2016-07-26 00:20:13 +0000

    stub

---
+++ stub_server/base_server.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'json'
+
+
+# "{\"node_id\":\"1234\",
+#\"readings\":[{\"temperature\":98.6,
+#\"humidity\":31.4,\"time_taken\":\"2016-10-04T12:12.123\"},
+#]}" 
+
+
+# list all
+get '/widgets' do
+  "test"
+end
+
